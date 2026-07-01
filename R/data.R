99d36ab24dbf3fa7ab7c6792d95f2e164a2f5f61
Jess Hsing <jcc.hsing@neurophysiology.nl>
2026-07-01 15:02:11 -0400

    Add framingbinaried dataset

--- R/data.R
+++ R/data.R
@@ -61,3 +61,69 @@
 #'
 #' @source Simulated data included with the \pkg{buzzMed} package.
 "sublongspikes"
+
+#' Binarized Framing Dataset
+#'
+#' A modified version of the \code{framing} dataset from the
+#' \pkg{mediation} package, originally based on the study by
+#' Brader, Valentino, and Suhay (2008). The candidate mediators have
+#' been dichotomized for use with the binary mediator models implemented
+#' in \pkg{buzzMed}. The dataset contains 265 observations and 10 variables.
+#'
+#' @format A data frame with 265 rows and 10 variables:
+#' \describe{
+#'   \item{tone}{Binary treatment indicator denoting whether the news story
+#'   was framed positively or negatively. Can be used as a predictor.}
+#'
+#'   \item{eth}{Binary treatment indicator denoting whether the news story
+#'   featured a Latino or European immigrant. Can be used as a predictor.}
+#'
+#'   \item{treat}{Interaction of \code{tone} and \code{eth}. In the original
+#'   study, this interaction represented the significant treatment effect.
+#'   Can be used as a predictor.}
+#'
+#'   \item{emo}{Binary indicator of negative emotional response. A value of
+#'   1 indicates an original score of 7 or lower, corresponding to stronger
+#'   negative emotion. Can be used as a candidate mediator.}
+#'
+#'   \item{p_harm}{Binary indicator of perceived harm caused by increased
+#'   immigration. A value of 1 indicates an original score of 6 or higher,
+#'   corresponding to greater perceived harm. Can be used as a candidate
+#'   mediator.}
+#'
+#'   \item{immigr}{Binary indicator derived from the original four-point
+#'   immigration attitude scale. A value of 1 indicates an original score
+#'   of 3 or 4, corresponding to more negative attitudes toward increased
+#'   immigration. Can be used as a candidate mediator.}
+#'
+#'   \item{anx}{Binary indicator derived from the original four-point anxiety
+#'   scale. A value of 1 indicates respondents who reported being
+#'   ``very anxious'' or ``somewhat anxious'' about increased immigration.
+#'   Can be used as a candidate mediator.}
+#'
+#'   \item{english}{Binary indicator derived from the original four-point
+#'   scale measuring support for making English the official language of the
+#'   United States. A value of 1 indicates respondents who selected
+#'   ``Favor'' or ``Strongly Favor.'' Can be used as a candidate mediator.}
+#'
+#'   \item{cong_mesg}{Binary outcome indicating whether the respondent
+#'   requested that an anti-immigration message be sent to Congress on
+#'   their behalf.}
+#'
+#'   \item{anti_info}{Binary outcome indicating whether the respondent
+#'   requested additional information from anti-immigration organizations.}
+#' }
+#'
+#' @references
+#' Brader, T., Valentino, N. A., & Suhay, E. (2008).
+#' What Triggers Public Opposition to Immigration? Anxiety, Group Cues,
+#' and Immigration Threat.
+#' \emph{American Journal of Political Science}, 52(4), 959--978.
+#'
+#' @source
+#' Adapted from the \code{framing} dataset in the \pkg{mediation} package.
+#'
+#' @docType data
+#' @name framingbinarized
+#' @keywords datasets
+"framingbinarized"
